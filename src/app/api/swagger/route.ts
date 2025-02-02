8782e1f7667229c18aff0513c68f1739f77c089e
vrravikant <ravikant@valueresearch.in>
2025-02-02 21:50:17 +0530

    Added the Swagger API documentation

--- src/app/api/swagger/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET(request: Request) {
-  return new Response('Hello Its working', {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-}
